72c02f18130a98e0f5f8c87610f3cf193b5cbeed
Praneetha Kalbhavi <praneethakalbhavi@gmail.com>
2022-11-10 16:25:52 +0530

    Updating files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ɵɵngDeclareClassMetadata } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'counterapp';
+  title = 'Counter App';
+  count = 0;
+  increase() {
+    this.count = this.count + 1;
+  }
+  decrease() {
+    this.count = this.count - 1;
+  }
 }
